b90ebdba53bd4f82c82b86f5c327981c50a14bed
Anurag <anurag1822003@gmail.com>
2026-02-08 18:50:50 +0530

    Add Dockerfile for deployment

--- server/server.js
+++ server/server.js
@@ -35,6 +35,6 @@ app.post("/run", (req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on", PORT));
+
